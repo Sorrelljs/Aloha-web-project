612ebb563c7981303dde360b3c961638ac7a445c
Sorrelljs <[sjsohota@gmail.com]>
2020-02-13 18:13:38 -0800

    subscribe email verification

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Aloha-web-project
+Website Aloha Apparel 
 
-A static Apparel website.
 
 ## Description
-Here I made a single apparel page which is responsive to tablet, mobile and desktop. I used flickity for the carousel display and vanilla javascript for the subscribe feature. I also implemented an email verification to make sure it is a real email entered.
+Here I made a single apparel page which is tablet, desktop and mobile friendly.
 
 
 
-
-# Technology used
+## Technology
 * HTML/ CSS
 * Javascript
 * JQuery
--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
-<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+
+
+
 
 $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
     contain: true
-  });
+});
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll("button");
     const navLinks = document.querySelectorAll("nav li");
-    const form = document.querySelector('#form-sign-up');
+    const form = document.querySelector('.subscribe-form');
+
+
 
     //Smooth scroll
-    navLinks.forEach(function(element) {
+    navLinks.forEach(function (element) {
         // add click listener for the element in this iteration of the loop
-        element.addEventListener("click", function(event){
+        element.addEventListener("click", function (event) {
             event.preventDefault();
-        // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
+            // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
             let href = this.querySelector('a').hash;
-        // scroll to that section with .scrollIntoView()
-            document.querySelector(href).scrollIntoView({ 
-                behavior: 'smooth' 
+            // scroll to that section with .scrollIntoView()
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
             });
         });
     });
     // E-mail validation
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
-        let email = document.getElementById('input-field').value;
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
-            alert("Thanks for subscribing!")
-        } else{
-            alert("Please insert a valid email address.")
+
+    form.addEventListener('submit', e => {
+        handleEmail(e)
+    });
+
+    function emailIsValid(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+
+    function handleEmail(e) {
+        e.preventDefault();
+        const emailSent = e.target.elements[0].value;
+        if (emailIsValid(emailSent)) {
+            alert('Thanks for your subscription!');
+        } else {
+            alert('Your e-mail is incorrect. Please enter a valid email');
         }
-    });
+    }
 
-    // for(i = 0; i < buttons.length; i++){
-    //     buttons[i].addEventListener("click", clickCheck, false);
-    // }
 
-    // function clickCheck(){
-    //     if(this.classList.contains("add-to-cart")){
-    //         console.log("Clicked")
-    //     }
-    // }
 
-    // Init carousel
-    $('.main-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-    });
+
 });
 
 
